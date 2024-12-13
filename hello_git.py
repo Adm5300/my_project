bb514ceb3b79e5cc3fb793069b5d21df9429eb59
Adm5300 <adm5300@gmail.com>
2024-12-13 11:28:33 +0300

    Update hello_git.py

---
+++ hello_git.py
@@ -0,0 +1,21 @@
+import os
+import module
+
+print('Hello Git')
+print('Indexing - it is impotant')
+print('13.12.2024')
+print('Create new branch testing')
+
+for num in range(10):
+        print(num,end=' ')
+
+for num in range(10):
+        print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
+
+print(module.add_num(4 , 7))
+
