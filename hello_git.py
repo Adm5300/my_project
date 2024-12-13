89ef6cd666650384cf1d0474cf8d878859b894e8
Adm5300 <adm5300@gmail.com>
2024-12-13 12:16:51 +0300

    Create hello_git.py

--- hello_git.py
+++ hello_git.py
@@ -5,7 +5,7 @@ print('Hello Git')
 print('Indexing - it is impotant')
 print('13.12.2024')
 print('Create new branch testing')
-print('master')
+print('master ')
 
 for num in range(10):
         print(num,end=' ')
